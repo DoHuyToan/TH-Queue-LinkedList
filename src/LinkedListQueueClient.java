feb0d793d516c2729e72b041483c477ddcc1d162
DoHuyToan <dhtoan89@gmail.com>
2021-09-30 15:21:43 +0700

    ok

---
+++ src/LinkedListQueueClient.java
@@ -0,0 +1,14 @@
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        //tạo đối tượng queue thuộc lớp MyLinkedListQueue
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+    }
+}
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,9 @@
 public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key){    //constructor
+        this.key = key;
+        this.next = null;
+    }
 }
